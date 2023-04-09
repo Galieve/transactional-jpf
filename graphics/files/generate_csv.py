a21dd91476a0516a72cd7c96fbb6578c0a279129
Enrique <enrique@MacBook-Pro-de-Enrique.local>
2023-04-09 23:45:47 +0200

    graphics

---
+++ graphics/files/generate_csv.py
@@ -0,0 +1,200 @@
+import os
+import sys
+import pandas as pd
+import re
+
+from os import listdir
+from os.path import isfile, join, isdir
+# pd.options.mode.chained_assignment = None
+
+
+def get_path_file(filename, subfolder=""):
+    file_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(file_dir, subfolder, filename)
+    return file_path
+
+
+def load_csv(name, subfolder=""):
+    file_path = get_path_file(name, subfolder)
+    return pd.read_csv(file_path, sep=';')
+
+
+def getPath(folder):
+    os.chdir(os.path.expanduser("../../bin/benchmarks/"))
+    return os.getcwd() + "/" + folder
+
+
+def process_filename(item):
+    list_item = re.split('\.|:|\+', item)
+    item_map = {}
+    if list_item[3] == 'TrDFSearch':
+        item_map['Base ISO'] = list_item[7].replace('History', '')
+        item_map['True ISO'] = list_item[11].replace('History', '')
+    else:
+        item_map['Base ISO'] = 'Naive'
+        item_map['True ISO'] = list_item[11].replace('History', '')
+    return item_map
+
+
+def process_path_application(path):
+    list = re.split('/|-', path)
+    return list[len(list) - 3] + '-' + list[len(list) - 2]
+
+#list[len(list) - 6] + '-' + list[len(list) - 4]
+def index_path_tr(path):
+    list = re.split('/|-', path)
+    return list, 4
+
+def index_path_so(path):
+    list = re.split('/|-', path)
+    return list, 2
+
+
+def process_path_trso(path, parameter):
+    if parameter == 'Transaction':
+        list, id = index_path_tr(path)
+    else:
+        list, id = index_path_so(path)
+    result = {}
+    subcase = list[len(list) - 1 - id]
+    result['Subcase'] = subcase
+    result['Case'] = list[len(list) - 2 - id] + '-' + subcase[len(subcase) - 1]
+    return result
+
+
+def process_file(filename, item, results):
+    file = filename + "/" + item
+    f = open(file, "r")
+    # print(open(file, "r").read())
+
+    for line in f:
+        if line.startswith("elapsed time:"):
+            list = line.split(' ')
+            results['Time'] = list[len(list) - 1].replace('\n', '')
+        if line.startswith('states:'):
+            list = line.split(',')
+            results['End States'] = list[len(list) - 1].replace('\n', '').replace('end=', '')
+        if line.startswith('transactional:'):
+            list = line.split(' ')
+            list = list[len(list) - 1].split(',')
+            results['Histories'] = list[0].replace('\n', '').replace('histories=', '')
+        if line.startswith('max memory:'):
+            list = line.split(' ')
+            results['Memory'] = list[len(list) - 1].replace('\n', '').replace('MB', '')
+
+    if len(results) <= 3:
+        return {}
+    return results
+
+def process_file_tr_so(filename, item, parameter):
+    results = process_path_trso(filename, parameter)
+    file, id = item
+    results[parameter] = id
+    return process_file(filename, file, results)
+
+
+def process_file_application(filename, item):
+
+    results = process_filename(item)
+    results['Case'] = process_path_application(filename)
+    return process_file(filename, item, results)
+
+def listFilesApplication(folder):
+    path = getPath(folder)
+    benchmark_names = [f for f in listdir(path) if isdir(join(path, f))]
+    cases = {}
+    files = {}
+    for b in benchmark_names:
+        path_benchmark = path + "/" + b
+        cases[b] = [f for f in listdir(path_benchmark) if isdir(join(path_benchmark, f))]
+        for c in cases[b]:
+            path_case = path_benchmark + "/" + c
+            files[path_case] = [f for f in listdir(path_case) if isfile(join(path_case, f))]
+
+    df = pd.DataFrame(columns=['Case', 'Base ISO', 'True ISO', 'Histories', 'End States', 'Time', 'Memory'])
+
+    for path, item in files.items():
+        for i in item:
+            results = process_file_application(path, i)
+            if results != {}:
+            #print(results)
+            #print(df.shape)
+
+                df_results = pd.DataFrame([results])
+                df = pd.concat([df, df_results], ignore_index=True)
+
+    #print(df.head())
+    df = df.sort_values(by=['Case', 'Base ISO', 'True ISO'])
+    df.to_csv(folder + '/data.csv', index=False, encoding='utf-8', sep=";")
+
+def listFilesThSeScalability(folder, parameter):
+    path = getPath(folder)
+    benchmark_names = [f for f in listdir(path) if isdir(join(path, f))]
+    cases = {}
+    subcases = {}
+    files = {}
+    for b in benchmark_names:
+        path_benchmark = path + "/" + b
+        cases[b] = [f for f in listdir(path_benchmark) if isdir(join(path_benchmark, f))]
+        for c in cases[b]:
+            path_case = path_benchmark + "/" + c
+            subcases[path_case] = [f for f in listdir(path_case) if isdir(join(path_case, f))]
+            for sc in subcases[path_case]:
+                path_subcase = path_case + "/" + sc
+                idx = sc[0]
+                files[path_subcase] = [(f, idx) for f in listdir(path_subcase) if isfile(join(path_subcase, f))]
+
+    df = pd.DataFrame(columns=['Case', parameter, 'Histories', 'End States', 'Time', 'Memory'])
+
+    for path, item in files.items():
+        for i in item:
+            results = process_file_tr_so(path, i, parameter)
+            if results != {}:
+                print(results)
+            print(df.shape)
+
+            df_results = pd.DataFrame([results])
+            df = pd.concat([df, df_results], ignore_index=True)
+
+    print(df.head())
+    df = df.sort_values(by=['Case', parameter, 'Subcase'])
+    df.to_csv(folder + '/data.csv', index=False, encoding='utf-8', sep=";")
+
+
+# print(path + " -> " + ' '.join(item))
+
+def generate_depending_on_mode(mode):
+    if mode.endswith('application-scalability'):
+        listFilesApplication(mode)
+    elif mode.endswith('session-scalability'):
+        listFilesThSeScalability(mode, 'Session')
+    elif mode.endswith('transaction-scalability'):
+        listFilesThSeScalability(mode, 'Transaction')
+
+
+def process_extra():
+    os.chdir(os.path.expanduser("../../"))
+
+    file = os.getcwd() + "/extra-cases.sh"
+    f = open(file, "r")
+    # print(open(file, "r").read())
+
+    with open('extra-cases2.sh', 'w') as fw:
+        for line in f:
+            fw.write("echo " + line)
+            fw.write(line)
+            fw.write("")
+
+
+
+
+
+if __name__ == "__main__":
+    folder = sys.argv[1]
+
+    print('Number of arguments:', len(sys.argv), 'arguments.')
+    print('Argument List:', str(sys.argv))
+
+    generate_depending_on_mode(folder)
+
+
---
+++ graphics/files/graphics-docker.py
@@ -0,0 +1,393 @@
+import csv
+import os
+import sys
+import time
+from datetime import datetime
+from math import log10
+
+import pandas
+from matplotlib.ticker import MaxNLocator
+
+import pandas as pd
+import ast
+import numpy as np
+import warnings
+import matplotlib.pyplot as plt
+from matplotlib import collections as matcoll
+import matplotlib.ticker as tick
+from textwrap import wrap
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+pd.options.mode.chained_assignment = None
+
+
+def get_path_file(filename, subfolder=""):
+    file_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(file_dir, subfolder, filename)
+    return file_path
+
+
+def getPath(folder):
+    os.chdir(os.path.expanduser("../../bin/benchmarks/"))
+    return os.getcwd() + "/" + folder
+
+
+def load_csv(name, subfolder=""):
+    file_path = get_path_file(name, subfolder)
+    return pd.read_csv(file_path, sep=';')
+
+
+def set_font(names):
+    plt.rcParams["font.family"] = names
+
+
+def time_to_int(time_string):
+    h, m, s = time_string.split(':')
+    return int(h) * 3600 + int(m) * 60 + int(s)
+
+
+def time_to_int_nan(time_string):
+    t = time_to_int(time_string)
+    if t >= 1800 - 1:
+        return np.nan
+    else:
+        return t
+
+
+def values_if_nan(h, e, t, m):
+    if np.isnan(t):
+        return np.nan, np.nan, np.nan
+    else:
+        return int(h), int(e), int(m)
+
+
+def prepare_dataframe_cactus(file, n):
+    df = load_csv(file + ".csv")
+
+    df['Time'] = df['Time'].map(lambda x: time_to_int_nan(x) / 60)
+    df['Histories'], df['End States'], df['Memory'] = \
+        df.apply(lambda r: values_if_nan(r['Histories'], r['End States'], r['Time'], r['Memory']), axis=1).str
+
+    df['Histories'] = df['Histories'].map(lambda x: x / 1000)
+    df['End States'] = df['End States'].map(lambda x: x / 1000)
+    df['Memory'] = df['Memory'].map(lambda x: (x) / 1024)
+
+    return process_dataframe_application(df)
+
+
+def prepare_dataframe_parameters(file, n):
+    df = load_csv(file + ".csv")
+    df['Time'] = df['Time'].map(lambda x: time_to_int_nan(x))
+    df['Histories'], df['End States'], df['Memory'] = \
+        df.apply(lambda r: values_if_nan(r['Histories'], r['End States'], r['Time'], r['Memory']), axis=1).str
+
+    df['Histories'] = df['Histories'].map(lambda x: x / 1000)
+    df['End States'] = df['End States'].map(lambda x: x / 1000)
+    df['Memory'] = df['Memory'].map(lambda x: x)
+
+
+def prepare_dataframe_scalability(file, parameter):
+    df = load_csv(file + ".csv")
+
+    df['Time'] = df['Time'].map(lambda x: time_to_int(x))
+    df['Histories'], df['End States'], df['Memory'] = df.apply(
+        lambda r: values_if_nan(r['Histories'], -1, r['Time'], r['Memory']), axis=1).str
+
+    df['Histories'] = df['Histories'].map(lambda x: x / 1000)
+    df['Memory'] = df['Memory'].map(lambda x: x / 1024)
+    return process_dataframe_trso(df, parameter)
+
+
+def process_dataframe_application(df):
+    labels_app = [('Causal', 'Causal'), ('Causal', 'SnapshotIsolation'),
+                  ('Causal', 'Serializable'), ('ReadAtomic', 'Causal'),
+                  ('ReadCommitted', 'Causal'), ('Trivial', 'Causal'),
+                  ('Naive', 'Causal')]
+
+    i = 1
+    # 'Case', 'Base ISO', 'True ISO', 'Histories', 'End States', 'Time', 'Memory'
+    other_df = pd.DataFrame()
+    other_df['Benchmark'] = df['Case']
+    other_df = other_df.drop_duplicates()
+    other_df.set_index('Benchmark')
+
+    i = 1
+    for base, true in labels_app:
+        aux_df = df.loc[(df['Base ISO'] == base) & (df['True ISO'] == true)]
+        other_df['Histories' + str(i)] = aux_df['Histories'].values
+        other_df['Time' + str(i)] = aux_df['Time'].values
+        other_df['Mem.' + str(i)] = aux_df['Memory'].values
+        other_df['End states' + str(i)] = aux_df['End States'].values
+
+        i += 1
+    other_df = other_df.reset_index()
+    return other_df
+
+
+def process_dataframe_trso(df, parameter):
+    labels_app = ['1', '2', '3', '4', '5']
+
+    i = 1
+    # 'Case', 'Base ISO', 'True ISO', 'Histories', 'End States', 'Time', 'Memory'
+    other_df = pd.DataFrame()
+    other_df['Benchmark'] = df['Case']
+    other_df = other_df.drop_duplicates()
+    other_df.set_index('Benchmark')
+
+    i = 1
+    for case in labels_app:
+        aux_df = df.loc[(df[parameter] == i)]
+        other_df['Histories' + str(i)] = aux_df['Histories'].values
+        other_df['Time' + str(i)] = aux_df['Time'].values
+        other_df['Mem.' + str(i)] = aux_df['Memory'].values
+        other_df['End states' + str(i)] = aux_df['End States'].values
+
+        i += 1
+
+    return other_df
+
+
+
+def plot_cactus(file, saved_file, field, labels):
+    n = len(labels) + 1
+
+    df = prepare_dataframe_cactus(file, n)
+
+
+    fig = plt.figure()
+    # plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=None, hspace=None)
+
+    ax = fig.add_axes((0.1, 0.1, 0.85, 0.85))
+
+    plt.rc('figure', titlesize=20)
+    ax.set_xlabel('Number of benchmarks')
+    ax.set_ylabel('Time (min)')
+    plt.gca().set_prop_cycle(plt.cycler('color', plt.cm.get_cmap('Dark2_r')(np.linspace(0, 1, n))))
+    plt.xlim([0 - 0.25, len(df.index) - 1 + 0.25])
+
+    for i_s in range(1, n):
+        i = str(i_s)
+        algo = df[['Benchmark', 'Histories' + i, 'End states' + i, 'Time' + i, 'Mem.' + i]]
+        algo = algo.sort_values(by=[field + i])
+        algo['Time' + i] = algo['Time' + i].cumsum()
+        #algo = algo.dropna(subset='Time'+i)
+        # lines = [(i, t) for (i,t) in zip( algo['Time'+i], df.index)]
+        plt.plot(df.index, algo[field + i], label=labels[i_s - 1], linewidth=2)
+
+    plt.legend(loc="lower right", borderpad=0.1, fontsize=11)
+
+    #plt.show()
+    print('Saving...')
+    fig.savefig(saved_file + '.eps', format='eps', bbox_inches='tight')
+
+    print('Saved! :)')
+
+    return
+
+
+def plot_cactus_mem(file, saved_file, field, labels):
+    n = len(labels) + 1
+
+    df = prepare_dataframe_cactus(file, n)
+
+    fig = plt.figure()
+    ax = fig.add_axes((0.15, 0.15, 0.8, 0.8))
+
+    plt.rc('figure', titlesize=20)
+    # plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=None, hspace=None)
+
+    ax.set_xlabel('Number of benchmarks')
+    ax.set_ylabel('Memory (GB)')
+    plt.gca().set_prop_cycle(plt.cycler('color', plt.cm.get_cmap('Dark2_r')(np.linspace(0, 1, n))))
+    plt.xlim([0 - 0.25, len(df.index) - 1 + 0.25])
+    plt.ylim([0 - 0.25, 32 + 0.25])
+
+    for i_s in range(1, n):
+        i = str(i_s)
+        algo = df[['Benchmark', 'Histories' + i, 'End states' + i, 'Time' + i, 'Mem.' + i]]
+        algo = algo.sort_values(by=[field + '.' + i])
+        algo['Mem' + i] = algo['Mem.' + i].cumsum()
+        # print(algo)
+        # lines = [(i, t) for (i,t) in zip( algo['Time'+i], df.index)]
+
+        plt.plot(df.index, algo[field + i], label=labels[i_s - 1], linewidth=2)
+
+    plt.legend(loc="lower right", borderpad=0.1, fontsize=11)
+
+    print('Saving...')
+
+    fig.savefig(saved_file + '.eps', format='eps', bbox_inches='tight')
+    # plt.show()
+
+    print('Saved! :)')
+
+    return
+
+
+def plot_cactus_histories(file, saved_file, labels):
+    n = len(labels) + 1
+
+    df = prepare_dataframe_cactus(file, n)
+
+    fig = plt.figure()
+    ax = fig.add_axes((0.1, 0.1, 0.85, 0.85))
+
+    plt.rc('figure', titlesize=20)
+    # plt.subplots_adjust(left=0.1, bottom=None, right=None, top=None, wspace=None, hspace=None)
+
+    ax.set_xlabel('Number of benchmarks')
+    ax.set_ylabel('Number of histories ($\\times 10^3$)')
+    plt.gca().set_prop_cycle(plt.cycler('color', plt.cm.get_cmap('Dark2_r')(np.linspace(0, 1, n))))
+    plt.xlim([0 - 0.25, len(df.index) - 1 + 0.25])
+
+    for i_s in range(1, n):
+        i = str(i_s)
+        if i_s > 1:
+            df_labels = ['Benchmark', 'Histories' + i, 'End states' + i, 'Time' + i, 'Mem.' + i, 'Histories1']
+        else:
+            df_labels = ['Benchmark', 'Histories' + i, 'End states' + i, 'Time' + i, 'Mem.' + i]
+        algo = df[df_labels]
+        if i_s <= 3:
+            field = 'Histories'
+        else:
+            field = 'End states'
+
+        algo = algo.sort_values(by=[field + i])
+
+        algo[field + i] = algo[field + i].cumsum()
+
+        plt.plot(df.index, algo[field + i], label=labels[i_s - 1], linewidth=2)
+
+    plt.legend(loc="upper right", borderpad=0.1, fontsize=11)
+
+    print('Saving...')
+    fig.savefig(saved_file + '.eps', format='eps', bbox_inches='tight')
+    # plt.show()
+
+    print('Saved! :)')
+
+    return
+
+
+def plot_scalability(file, parameter, saved_file, object_name):
+    n = 5 + 1
+    df = prepare_dataframe_scalability(file, parameter)
+
+    bench = len(df.index)
+    fig = plt.figure()
+    ax = fig.add_axes((0.15, 0.15, 0.85, 0.85))
+    ax2 = ax.twinx()
+
+    plt.rc('figure', titlesize=20)
+    ax.set_xlabel('Number of ' + object_name)
+    ax.set_ylabel('Time (mins)')
+    ax2.set_ylabel('Memory (GB)', labelpad=10)
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+    ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+    ax2.yaxis.set_major_locator(MaxNLocator(integer=True))
+
+    cmap = plt.cm.get_cmap('Dark2')
+
+    plt.xlim([1 - 0.05, 5 + 0.05])
+    ax2.set_ylim([0 - 0.25, 30 + 0.25])
+
+    ax2.set_ylim([0 - 0.25, 16 - 0.25])
+
+    # plt.ylim([0 - 0.25, 30 + 0.25])
+
+    memory = [df['Mem.' + str(i)].mean() for i in range(1, n)]
+    time = [df['Time' + str(i)].mean() / 60 for i in range(1, n)]
+
+    t_lab = ax.plot(range(1, n), time, label='Avg. time', linewidth=4, color=cmap(4 / 8 - 0.05))
+    m_lab = ax2.plot(range(1, n), memory, label='Avg. memory', linewidth=4, color=cmap(1 / 8 - 0.05))
+    lns = t_lab + m_lab
+    labs = [l.get_label() for l in lns]
+    plt.legend(lns, labs, loc="upper left")
+
+    print('Saving...')
+    fig.savefig(saved_file + '.eps', format='eps', bbox_inches='tight')
+
+    print('Saved! :)')
+
+    return
+
+
+def calculate_parameters(file, labels):
+    n = len(labels) + 1
+
+    df = prepare_dataframe_parameters(file, n)
+
+    results = {}
+    for i_s in range(1, n):
+        i = str(i_s)
+        if i_s > 1:
+            algo = df[['Benchmark', 'Histories' + i, 'End states' + i, 'Time' + i, 'Mem.' + i, 'Time1']]
+            algo.loc[:, 'SpeedUp' + i] = algo['Time' + i] / algo['Time1']
+            results['SpeedUp' + i + '-average'] = algo['SpeedUp' + i].mean()
+        else:
+            algo = df[['Benchmark', 'Histories' + i, 'End states' + i, 'Time' + i, 'Mem.' + i]]
+
+            results['SpeedUp' + i + '-average'] = 1
+
+        results['Time' + i + '-average'] = algo['Time' + i].map(lambda x: x).mean().squeeze()
+
+        # results['Time'+i+'-min'] = algo['Time'+i].min()
+        # results['Time'+i+'-max'] = algo['Time'+i].max()
+        # results['SpeedUp'+i+'-min'] = algo['SpeedUp'+i].min()
+        # results['SpeedUp'+i+'-max'] = algo['SpeedUp'+i].max()
+        # results['Mem.'+i+'-min'] = algo['Mem.'+i].min()
+        # results['Mem.'+i+'-max'] = algo['Mem.'+i].max()
+        results['Mem.' + i + '-average'] = algo['Mem.' + i].mean()
+        if i_s <= 3:
+            field = 'Histories'
+        else:
+            field = 'End states'
+
+        algo[field + i] = algo[field + i].map(lambda x: x)
+
+        results['Histories' + i + '-average'] = algo[field + i].mean()
+        # results['Histories'+i+'-min'] = algo[field+i].min()
+        # results['Histories'+i+'-max'] = algo[field+i].max()
+
+    results = {k: "%.2f" % v for k, v in results.items()}
+
+    with open('results.csv', 'w') as f:  # You will need 'wb' mode in Python 2.x
+        w = csv.DictWriter(f, results.keys())
+        w.writeheader()
+        w.writerow(results)
+
+
+def calculate_parameters_2(file):
+    n = 5 + 1
+    df = prepare_dataframe_scalability(file, n)
+
+    memory = [df['Mem.' + str(i)].map(lambda x: x * 1024).mean() for i in range(1, n)]
+    print(np.array(memory).mean())
+
+
+def plot_depending_on_mode(mode):
+    folder = getPath(mode)
+    if mode.endswith('application-scalability'):
+        plot_cactus(folder + '/data', folder + '/app-scala-Time', 'Time', labels)
+        plot_cactus_mem(folder + '/data', folder + '/app-scala-Mem', 'Mem', labels)
+        plot_cactus_histories(folder + '/data', folder + '/app-scala-histories', labels)
+    elif mode.endswith('session-scalability'):
+        plot_scalability(folder + '/data', 'Session', folder + '/se-scala', 'sessions')
+    elif mode.endswith('transaction-scalability'):
+        plot_scalability(folder + '/data', 'Transaction',  folder + '/tr-scala', 'transactions per session')
+
+
+
+
+if __name__ == "__main__":
+    font = {'family': 'serif', 'size': 16, 'serif': ['computer modern roman']}
+    plt.rc('font', **font)
+    plt.rc('legend', **{'fontsize': 14})
+    labels = ["\\texttt{CC}", "\\texttt{CC} + \\texttt{SI}",
+              "\\texttt{CC} + \\texttt{SER}", "\\texttt{RA} + \\texttt{CC}",
+              "\\texttt{RC} + \\texttt{CC}", "\\texttt{true} + \\texttt{CC}", "DFS(\\texttt{CC})"]
+
+    mode = sys.argv[1]
+
+    plot_depending_on_mode(mode)
+
